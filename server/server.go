fd59c32a3d44189ef98ebf9c45c45b3f2655bb21
namcnab <nnekaxmcnab@gmail.com>
2025-03-10 15:37:11 -0700

    add BroadcastLetter RPC and related message types for server streaming email broadcasts

--- server/server.go
+++ server/server.go
@@ -17,3 +17,23 @@ func (s *EmailMessageServer) SendEmail(ctx context.Context, req *emailpb.EmailRe
 
 	return &emailpb.GenericResponse{MessageId: []byte(id), Message: req.GetSubject() + " was sent successfully!"}, nil
 }
+
+func (s *EmailMessageServer) BroadcastLetter(letter *emailpb.LetterMessage, stream emailpb.EmailMessageService_BroadcastLetterServer) error {
+	//generate uuid
+
+	recipients := []string{"user1@example.com", "user2@example.com", "user3@example.com"}
+
+	for _, recipient := range recipients {
+		id := uuid.New().String()
+		broadcast := &emailpb.Broadcast{
+			BroadcastId: []byte(id),
+			Sender:      letter.Sender,
+			Subscriber:  recipient,
+			Message:     letter.Message,
+		}
+		stream.Send(broadcast)
+	}
+
+	return nil
+
+}
